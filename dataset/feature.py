45bffdd084f08597aadefcbd560e3a9a592b2776
Untitled-W <2995000455@qq.com>
2025-07-22 23:19:53 +0800

    add preprocessors

---
+++ dataset/feature.py
@@ -0,0 +1,10 @@
+# add feature
+# input: HumanSequenceData
+# output: M x N x d
+
+# generate camera view
+
+# texture augment
+
+# render DFF
+
---
+++ dataset/retarget.py
@@ -0,0 +1,7 @@
+# retarget
+# input: HumanSequenceData
+# output: T x n_dof, T x 6
+
+# AnyTeleop
+
+# ManipTrans
